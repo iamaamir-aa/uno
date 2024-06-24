b254d31fafbffc3b5ed2cdb88dd29c662d3e3986
iamaamir-aa <mohdaamir7674@gmail.com>
2024-06-24 14:42:35 +0530

    tailwind setup, only required assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { totalCards } from './constants/constants';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'uno';
+  items = ['1', '2', '3', '4', '5'];
+  totalCards = totalCards;
+  cardsPerPlayer = 7;
+  getXRandomCards() {
+
+  }
+}
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+function getXRandomCards(x: number) {
+  let cards = [];
+  for (let i = 0; i < x; i++) {
+    cards.push(getRandomInt(totalCards));
+  }
+  return cards;
 }
---
+++ src/app/constants/constants.ts
@@ -0,0 +1,3 @@
+export const totalCards = 54;
+export const totalPlayers = 2;
+export const cardsPerPlayer = 7;
